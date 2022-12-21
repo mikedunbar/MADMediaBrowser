0a7d24da2f3c53445b8337bc5cec315615a22e63
Mike Dunbar <mdunbar@us.ibm.com>
2022-12-21 14:27:34 -0500

    create paid n free flavors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,21 +5,58 @@ plugins {
     id("dagger.hilt.android.plugin")
 }
 
-val compose_version by extra("1.3.2")
-val compose_material_version by extra("1.3.1")
-val hilt_version by extra("2.44.2")
-val retrofit_version by extra("2.9.0")
-val moshi_version by extra("1.13.0")
-val coroutines_version by extra("1.5.2")
-val ktx_version by extra("2,5,1")
+val composeVersion by extra("1.3.2")
+val composeMaterialVersion by extra("1.3.1")
+val hiltVersion by extra("2.44.2")
+val retrofitVersion by extra("2.9.0")
+val moshiVersion by extra("1.13.0")
+val coroutinesVersion by extra("1.5.2")
+val ktxVersion by extra("2.5.1")
 
 android {
-    compileSdk = 33
+    compileSdk = rootProject.extra["compile_sdk_version"] as Int
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+        viewBinding = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = composeVersion
+    }
+
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+    namespace = "dunbar.mike.musicbrowser"
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
 
     defaultConfig {
         applicationId = "dunbar.mike.musicbrowser"
-        minSdk = 23
-        targetSdk = 33
+        minSdk = rootProject.extra["min_sdk_version"] as Int
+        targetSdk = rootProject.extra["target_sdk_version"] as Int
         versionCode = 1
         versionName = "1.0"
 
@@ -30,37 +67,20 @@ android {
         }
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+    flavorDimensions += "tier"
+    productFlavors {
+        create("free") {
+            dimension = "tier"
+            applicationId = "dunbar.mike.musicbrowser.free"
+            versionNameSuffix = "-free"
+        }
 
+        create("paid") {
+            dimension = "tier"
+            applicationId = "dunbar.mike.musicbrowser.paid"
+            versionNameSuffix = "-paid"
         }
     }
-    compileOptions {
-        isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-        viewBinding = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = compose_version
-    }
-    packagingOptions {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    namespace = "dunbar.mike.musicbrowser"
 }
 
 dependencies {
@@ -72,22 +92,21 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.7.0")
-    implementation("androidx.compose.ui:ui:$compose_version")
-    implementation("androidx.compose.material:material:$compose_material_version")
-    implementation("androidx.compose.material:material-icons-extended:$compose_material_version")
-    implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.material:material:$composeMaterialVersion")
+    implementation("androidx.compose.material:material-icons-extended:$composeMaterialVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     implementation("androidx.navigation:navigation-compose:2.6.0-alpha04")
     implementation("androidx.activity:activity-compose:1.6.1")
 
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$ktx_version")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$ktx_version")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$ktxVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$ktxVersion")
 
-    implementation("com.google.dagger:hilt-android:$hilt_version")
-    kapt("com.google.dagger:hilt-compiler:$hilt_version")
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-compiler:$hiltVersion")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
-
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
 
     //region OkHttp
     // define a BOM and its version
@@ -98,20 +117,20 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor")
     //endregion
 
-    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
-    implementation("com.squareup.retrofit2:converter-moshi:$retrofit_version")
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-moshi:$retrofitVersion")
 
 
     // Moshi
-    implementation("com.squareup.moshi:moshi:$moshi_version")
-    kapt("com.squareup.moshi:moshi-kotlin-codegen:$moshi_version")
+    implementation("com.squareup.moshi:moshi:$moshiVersion")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:$moshiVersion")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$compose_version")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
 
-    debugImplementation("androidx.compose.ui:ui-tooling:$compose_version")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
 }
 
 // Allow references to generated code
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    extra.apply {
-
-    }
     repositories {
         google()
         mavenCentral()
@@ -12,6 +9,12 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.42")
     }
+}
+
+ext {
+    extra["compile_sdk_version"] = 33
+    extra["min_sdk_version"] = 23
+    extra["target_sdk_version"] = 33
 }
 
 tasks.register("clean").configure {
